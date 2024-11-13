daf1cf2cfab89366aeadc6473424be841ad9148e
Ricardo Pérez <ricpelo@gmail.com>
2024-11-13 21:23:45 +0100

    Crea la tabla departamentos y ruta /departamentos

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/departamentos', function () {
+    return view('departamentos.index');
+});
+
 require __DIR__.'/auth.php';
