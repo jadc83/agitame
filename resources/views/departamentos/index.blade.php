348d9809744e39bef902462d6380ea048318008c
Ricardo Pérez <ricpelo@gmail.com>
2024-11-18 20:40:16 +0100

    Visualizar los departamentos

--- resources/views/departamentos/index.blade.php
+++ resources/views/departamentos/index.blade.php
@@ -1,9 +1,52 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Departamentos
+        </h2>
+    </x-slot>
+
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Hola
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Código
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Denominación
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Localidad
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <span class="sr-only">Editar</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($departamentos as $departamento)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $departamento->codigo }}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ $departamento->denominacion }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $departamento->localidad }}
+                                        </td>
+                                        <td class="px-6 py-4 text-right">
+                                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
